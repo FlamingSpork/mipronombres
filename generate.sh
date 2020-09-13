d6c45962a4765feede80ede3625d1209c3ba0988
FlamingSpork <philo_bunny@yahoo.com>
2020-09-13 17:48:26 -0400

    Added debug info to script

--- generate.sh
+++ generate.sh
@@ -4,5 +4,6 @@ for f in $(find -name "*.php")
 do
     # this grabs the filename without the last extension
     fn="${f%.*}"
+    echo "Building $f to $fn.html"
     php "$f" > "$fn.html"
 done
--- header.php
+++ header.php
@@ -11,6 +11,7 @@ print '
     border-right: 0; color: #bbb; text-align: left; margin: 0; padding: 0; width: 100%;
     position: relative; left: 0; top: 0; font-family: Sans-Serif; display: block; margin: calc(-50vw + 50%);
     width: 100vw;">
-    <img style="width: 50px; height: 50px;" src="logo.png">
-    <span style="margin: 0; position: absolute; top: 50%; -ms-transform: translateY(-50%); transform: translateY(-50%);"><a style="color: #bbb; text-decoration: none;" href="index.html">mipronombre.es</a></span>
+    <img style="width: 50px; height: 50px;" src="//mipronombre.es/logo.png">
+    <span style="margin: 0; position: absolute; top: 50%; -ms-transform: translateY(-50%); transform: translateY(-50%);"><a style="color: #bbb; text-decoration: none;" href="//mipronombre.es/index.html">mipronombre.es</a></span>
+</div>
 ';
